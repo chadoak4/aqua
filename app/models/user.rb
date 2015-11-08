2cb0dd47e587453d0a3db12c180002caef135542
Chad Oakley <Chad.Oakley@tampabay.rr.com>
2015-11-08 14:52:21 -0500

    changed user model to has_one attendee

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :cleanups
-  has_many :attendees
+  has_one :attendee
   devise   :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable,
            :omniauthable, :omniauth_providers => [:twitter]
