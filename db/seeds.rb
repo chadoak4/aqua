f8e3a375977935813ddd132de8058dc7a42b01b1
Chad Oakley <Chad.Oakley@tampabay.rr.com>
2015-11-11 10:54:00 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Cleanup.create(name:"Clearwater Beach", description: "The beach is filled with all kinds of plastic and tash. I have been to this beach a few times now and it keeps getting worse.", supplies: "gloves,trash bags", additionalinfo: "You may want to bring sunscreen. Even though its mid November it is still very hot!")
+Cleanup.create(name:"Indian Rocks Beach", description: "The beach hosted a festival over the weekend and the attendees trashed the beach.", supplies: "gloves,trash bags,rakes", additionalinfo: "I will supply lunch for anyone who comes. Please state you are attending so I can have an accurate count of people attending.")
+Cleanup.create(name:"St. Pete Beach", description: "Due to the recent hurricane a lot of debris and broken branches are scattered accross the beach", supplies: "gloves,trash bags,rakes", additionalinfo: "It would be awesome to have some people who can lift heavy object as their are some heavy objects that need to be removed.")
